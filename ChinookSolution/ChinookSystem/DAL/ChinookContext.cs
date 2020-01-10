f1d219d338f27008a37ee437d573af6e4f7aeea5
nloose1 <54917654+nloose1@users.noreply.github.com>
2020-01-10 16:44:24 -0700

    Finished BLL and DAL

--- ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
+++ ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Aditional namespaces
+using System.Data.Entity;
+using ChinookSystem.Data.Entities;
+#endregion
+
 namespace ChinookSystem.DAL
 {
-    class ChinookContext
+    //restrict access to the database interface to within the project
+    //inherit the Entity framework interface DbContext
+    internal class ChinookContext:DbContext
     {
+        //constructor will pass the connection string name to the database to EntityFramework via DbContext
+        public ChinookContext() : base("ChinookDB")
+        {
+        }
+
+        //create a DbSet<T> for each Enity
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; };
     }
 }
