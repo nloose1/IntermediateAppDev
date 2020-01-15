cb86cb0417becad4eee3b9ebc089550757ec380e
nloose1 <54917654+nloose1@users.noreply.github.com>
2020-01-15 14:32:47 -0700

    implemented user control

---
+++ ChinookSolution/ChinookSystem/DAL/BusinessRuleException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region AdditionalNamespace
+using System.Web;
+#endregion
+
+namespace DMIT2018Common.UserControls
+{
+    [Serializable]
+    public class BusinessRuleException : Exception
+    {
+        public List<string> RuleDetails { get; set; }
+        public BusinessRuleException(string message, List<string> reasons)
+            : base(message)
+        {
+            this.RuleDetails = reasons;
+        }
+    }
+}
